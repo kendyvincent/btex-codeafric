c7d259658382999e3d31cd7a42a1f5841f00f55d
Abodei Ebizimoh <vincentkendy@gmail.com>
2017-12-09 15:15:04 +0200

    Updates from Sunnepah

--- main.js
+++ main.js
@@ -8,20 +8,19 @@ coinApp.controller('mainController', function mainController($scope, $http) {
   $scope.total_amount_ngn = 0.0;
   $scope.total_units_usd = 0.0;
   $scope.total_units_ngn = 0.0;
-  $scope.currency = "NGN";
 
   $scope.getPrice = function(){
     console.log("Trying to get price");
     $http({
       method: 'GET',
-      url: 'https://api.coindesk.com/v1/bpi/currentprice/NGN.json '
+      url: 'https://api.coindesk.com/v1/bpi/currentprice/NGN.json'
     }).then(function successCallback(response) {
         var bpi = response.data.bpi
         $scope.rates.push(bpi['USD']);
         $scope.rates.push(bpi['NGN']);
         console.log($scope.rates);
       }, function errorCallback(response) {
-        console.error(response)
+        console.error(response);
       });
   }
 
@@ -29,7 +28,7 @@ coinApp.controller('mainController', function mainController($scope, $http) {
     angular.forEach($scope.rates, function(bt_rate) {
       console.log("calls", bt_rate.rate_float, bt_rate.code);
       var total = bt_rate.rate_float * parseInt($scope.units);
-      if (bt_rate.code == "USD") {
+      if (bt_rate.code == 'USD') {
         $scope.total_amount_usd = total;
       } else {
         $scope.total_amount_ngn = total;
@@ -39,14 +38,19 @@ coinApp.controller('mainController', function mainController($scope, $http) {
 
   $scope.calculateUnits = function() {
     angular.forEach($scope.rates, function(bt_rate) {
+
+      if(bt_rate.code == $scope.currency){
+
       var units = parseInt($scope.amount) / bt_rate.rate_float;
-      if ( $scope.currency == "USD") {
+      if (bt_rate.code == 'USD') {
+        console.log($scope.currency);
         $scope.total_units_usd = units;
         $scope.total_units_ngn = "0";
       } else {
         $scope.total_units_ngn = units;
         $scope.total_units_usd = "0";
       }
+    }
     });
   }
 });
