e54754dab45289b09f715bd3c53773dbb7e25146
Abodei Ebizimoh <vincentkendy@gmail.com>
2017-12-12 13:32:06 +0200

    Added new currencies

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
+
 var coinApp = angular.module('coinApp', []);
 
 coinApp.controller('mainController', function mainController($scope, $http) {
   $scope.title = "My Bitcoin Exchange";
   $scope.rates = [];
-  title = "My title";
+
   $scope.total_amount_usd = 0.0;
   $scope.total_amount_ngn = 0.0;
   $scope.total_units_usd = 0.0;
   $scope.total_units_ngn = 0.0;
 
-  $scope.getPrice = function(){
+  $scope.currency_to_convert_to = 'NGN';
+
+  $scope.getPrice = function() {
     console.log("Trying to get price");
     $http({
       method: 'GET',
-      url: 'https://api.coindesk.com/v1/bpi/currentprice/NGN.json'
+      url: 'https://api.coindesk.com/v1/bpi/currentprice/' + $scope.currency_to_convert_to + '.json'
     }).then(function successCallback(response) {
+      $scope.rates = [];
         var bpi = response.data.bpi
         $scope.rates.push(bpi['USD']);
-        $scope.rates.push(bpi['NGN']);
-        console.log($scope.rates);
+        $scope.rates.push(bpi[$scope.currency_to_convert_to]);
       }, function errorCallback(response) {
         console.error(response);
       });
@@ -26,9 +29,8 @@ coinApp.controller('mainController', function mainController($scope, $http) {
 
   $scope.calculateTotal = function() {
     angular.forEach($scope.rates, function(bt_rate) {
-      console.log("calls", bt_rate.rate_float, bt_rate.code);
       var total = bt_rate.rate_float * parseInt($scope.units);
-      if (bt_rate.code == 'USD') {
+      if (bt_rate.code == "USD") {
         $scope.total_amount_usd = total;
       } else {
         $scope.total_amount_ngn = total;
@@ -38,19 +40,16 @@ coinApp.controller('mainController', function mainController($scope, $http) {
 
   $scope.calculateUnits = function() {
     angular.forEach($scope.rates, function(bt_rate) {
-
-      if(bt_rate.code == $scope.currency){
-
-      var units = parseInt($scope.amount) / bt_rate.rate_float;
-      if (bt_rate.code == 'USD') {
-        console.log($scope.currency);
-        $scope.total_units_usd = units;
-        $scope.total_units_ngn = "0";
-      } else {
-        $scope.total_units_ngn = units;
-        $scope.total_units_usd = "0";
+      if (bt_rate.code == $scope.currency) {
+        var units = parseInt($scope.amount) / bt_rate.rate_float;
+        if (bt_rate.code == "USD") {
+          $scope.total_units_usd = units;
+          $scope.total_units_ngn = 0;
+        } else {
+          $scope.total_units_ngn = units;
+          $scope.total_units_usd = 0
+        }
       }
-    }
     });
   }
 });
